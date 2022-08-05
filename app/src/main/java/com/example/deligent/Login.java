112d2edc2a6d39be83ca2f6c3b40486351326e04
DEEPTI LODHI <deepti.lodhi.14@gmail.com>
2022-08-06 00:31:08 +0530

    this one

---
+++ app/src/main/java/com/example/deligent/Login.java
@@ -0,0 +1,4 @@
+package com.example.deligent;
+
+public class Login {
+}
--- app/src/main/java/com/example/deligent/MainActivity.java
+++ app/src/main/java/com/example/deligent/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.deligent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1,b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.button);
+        b2=(Button)findViewById(R.id.button2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
